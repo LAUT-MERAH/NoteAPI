dd852dcf95871304919dda6eb175a10aa33c6f5f
Chrystalio <chrystalio.k@outlook.com>
2024-12-02 20:09:26 +0700

    Add database connection and server setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connection = require('./config/db')
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.APP_PORT || 3000;
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        process.exit(1);
+    } else {
+        console.log('Connected to MySQL database');
+
+        app.listen(PORT, HOST, () => {
+            console.log(`Server is running on http://${HOST}:${PORT}`);
+        });
+    }
+});
